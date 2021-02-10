d5a9cb92368fec210ef45602b5cf8701d58d5634
BenGmaker <b.f.gortemaker@student.tue.nl>
2021-02-10 16:15:34 +0100

    Tried to change app color but failed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -20,9 +21,11 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber[650],
+        primaryColor: Colors.amber[650],
+
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Food drawer'),
     );
   }
 }
